ddbd3c89fd6369972f8c62e0d39c3f04a0815c97
Fr0s-T <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>
2025-07-26 19:10:30 +0300

    mplement user session management and login redirection
    - Added Session class to store the current logged-in User globally
    - Integrated Session.setCurrentUser in LoginPage upon successful login
    - Added User class with fields and a clean toString() method
    - Simplified user info printing using overridden toString()
    - Adjusted main logic to support login redirection based on user
      role

--- src/Classes/Main.java
+++ src/Classes/Main.java
@@ -3,6 +3,7 @@ package Classes;
 import javafx.application.Application;
 import Controllers.LoginPage;
 
+
 public class Main {
     public static void main(String[] args) {
         Application.launch(LoginPage.class, args);
