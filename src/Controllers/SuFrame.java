5bf216e95e78e89d1482dab2156759a4004b110f
Fr0s-T <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>
2025-07-28 21:25:58 +0300

    fixed log in issues sepreated loogic and changed application architure fixed sync issues

--- src/Controllers/SuFrame.java
+++ src/Controllers/SuFrame.java
@@ -3,10 +3,13 @@ package Controllers;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import Models.*;
+import java.util.Optional;
 
 /**
  * Author: @Frost
@@ -14,32 +17,47 @@ import javafx.stage.Stage;
  */
 public class SuFrame extends Application {
 
+    @FXML private ImageView UserImage;
+    @FXML private Label UsernameLabel;
+    @FXML private Label StatusLabel;
+    @FXML private Button AlertBtn;
     @FXML private Button LogoutBtn;
     private Stage mainStage;
 
     @FXML
-    private void initialize(){
-        LogoutBtn.setOnAction(actionEvent -> {
-            System.out.println("Button is pressed");
-        });
+    private void Logout(){
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout Cpnfirmation");
+        alert.setHeaderText("Are you sure you want to logout?");
+        alert.setContentText("You will be redirected to the login screen.");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            // Go back to login page
+            SceneLoader.loadScene("/FXML/LoginPage.fxml", null); // Adjust path if needed
+        }
+
+    }
+    @FXML
+    private void Initialize(){
+        User user=Session.getCurrentUser();
+        UsernameLabel.setText(user.getUsername());
+
+        LogoutBtn.setOnAction(event ->Logout());
+
     }
 
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) throws Exception{
         mainStage = stage;
-        // 1. Load the CORRECT FXML file
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/SuFrame.fxml")); // Changed from LoginPage.fxml
-        // 2. Set controller
-        loader.setController(this);
-        // 3. Load the root
-        Parent root = loader.load();
-        // 4. Setup scene
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/SuFrame.fxml"));
+        loader.setController(this); // Only if SuFrame is controller
+        AnchorPane root = loader.load();
+
         Scene scene = new Scene(root);
         stage.setScene(scene);
-        stage.setTitle("Main Application");
-        stage.show();
-    }
+        stage.show();    }
 
     public static void main(String[] args) {
         launch(args);
