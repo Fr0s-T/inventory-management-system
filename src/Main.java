e40221fedb527b95f6ddf45bb830098c0601a421
Fr0s-T <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>
2025-08-18 17:59:22 +0300

    added an icon

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import Models.Session;
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import Controllers.SceneLoader;
 import Services.ProductsService;
+
+import java.awt.*;
 
 /**
  *
@@ -18,6 +21,10 @@ public class Main extends Application {
             SceneLoader.setPrimaryStage(primaryStage);
             SceneLoader.loadScene("/FXML/LoginPage.fxml", null);
             primaryStage.setTitle("Inventory Management System");
+            primaryStage.getIcons().clear();
+            primaryStage.getIcons().add(new Image("/Icons/Possible logos/Screenshot__79_-removebg-preview.png"));
+
+
 
 
             primaryStage.setOnCloseRequest(event -> {
