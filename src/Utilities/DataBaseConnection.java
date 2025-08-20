8e487c6b8d87313dfe18cc565fc99f3fe1390499
Fr0s-T <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>
2025-08-20 20:09:19 +0300

    db now run on lical host changed the db connection class and .env locally

--- src/Utilities/DataBaseConnection.java
+++ src/Utilities/DataBaseConnection.java
@@ -14,22 +14,23 @@ import java.sql.SQLException;
 
 public class DataBaseConnection {
     private static final Dotenv dotenv = Dotenv.load();
-
     private static final String server = dotenv.get("DB_SERVER");
     private static final String database = dotenv.get("DB_NAME");
-    private static final String username = dotenv.get("DB_USER");
-    private static final String password = dotenv.get("DB_PASSWORD");
+    private static final String username = dotenv.get("DB_USER"); // Now used again
+    private static final String password = dotenv.get("DB_PASSWORD"); // Now used again
 
     public static String getConnectionUrl() {
         return "jdbc:sqlserver://" + server + ":1433;"
                 + "databaseName=" + database + ";"
                 + "encrypt=true;"
-                + "trustServerCertificate=false;"
+                + "trustServerCertificate=true;" // Changed to true like the demo
                 + "loginTimeout=30;";
+        // integratedSecurity is now removed
     }
 
     public static Connection getConnection() throws Exception {
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        // Now using username and password like the demo
         return DriverManager.getConnection(getConnectionUrl(), username, password);
     }
 }
